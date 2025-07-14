34f2ca2820c679017571ba31c20d7dd6b5bda818
Samrat <yashsamrat10080@gmail.com>
2025-07-14 22:17:46 +0530

    Improve panel buttons and logging

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,18 @@
+import logging
 from pyrogram import Client
+
+
+logger = logging.getLogger(__name__)
 
 
 def register_all(app: Client):
     from . import start, admin, autodelete, broadcast
 
     start.register(app)
+    logger.info("[LOADED] start.py registered")
     admin.register(app)
+    logger.info("[LOADED] admin.py registered")
     autodelete.register(app)
+    logger.info("[LOADED] autodelete.py registered")
     broadcast.register(app)
+    logger.info("[LOADED] broadcast.py registered")
